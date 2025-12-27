df8cf083c229c97ec14c05ac5472005a21ab6a3b
Vivesh <vivesht07@gmail.com>
2025-12-27 15:52:07 +0000

    basic-level task

---
+++ Basic-Level/6th.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo -e "Going back to the previous directory using cd - \n "
+
+cd -
+
+echo -e " \n " # Reason we are using \n is to add new inline
+
+echo -e "Navigating to Current Working Directory \n "
+
+pwd
+
