2a71d0d729718a74d21e0f854c51fa451ca200b3
Vivesh <vivesht07@gmail.com>
2025-12-27 15:51:17 +0000

    basic-level task

---
+++ Basic-Level/5th.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "To Nagivate to /tmp directory we need to use cd command"
+
+cd /tmp
+
+echo "check status use pwd"
+
+pwd
+
+echo "To Make sure your Working directory change to /tmp you need to use . 5th.sh or source 5th.sh"
+
