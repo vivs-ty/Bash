93b52673883268397f3f9bc4db3c9bfc8d67774b
Vivesh <vivesht07@gmail.com>
2026-01-17 14:02:42 +0000

    task 20th completed

---
+++ Tasks/Basic-Level/20th.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Remove the backup_scripts directory and its contents. (Use with caution!)
+
+if [[ -d backup_scripts ]]; then
+  rm -rf backup_scripts
+  echo "backup_scripts directory and its contents removed"
+else
+  echo "backup_scripts directory does not exist"
+  exit 1
+fi
