0bb08bcc9ea9ea74ab7faec6a8a700534e0f3a9a
Vivesh <vivesht07@gmail.com>
2026-01-17 13:43:12 +0000

    task 14th completed

--- Tasks/Basic-Level/14th.sh
+++ Tasks/Basic-Level/14th.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Enter the names of the files to create (separated by spaces):"
-read -r -a file_names
+read -r -a file_names # multiple files 
 
 echo "Creating files: ${file_names[*]}"
 touch "${file_names[@]}"
