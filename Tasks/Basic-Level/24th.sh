e3d0f31e2fc6feb155bef44c2aa74b866bbbfdf9
Vivesh <vivesht07@gmail.com>
2026-01-18 13:57:05 +0000

    task 24th completed

---
+++ Tasks/Basic-Level/24th.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILE="server.log"
+
+if [ -f "$FILE" ]; then
+    echo "Opening $FILE page by page. Press 'q' to quit."
+    sleep 2
+    less "$FILE"
+    #more "$FILE"
+else
+    echo "Error: $FILE not found."
+fi
