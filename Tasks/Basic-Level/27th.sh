7fe75a56ed44de0baa01e1c0f8d7eed815198169
Vivesh <vivesht07@gmail.com>
2026-01-18 14:22:22 +0000

    task 27th completed

---
+++ Tasks/Basic-Level/27th.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Example: setting the variable
+file="db.config"
+
+if [ -f "$file" ]; then
+    line_count=$(wc -l < "$file")
+    echo "The file '$file' has $line_count lines."
+else
+    echo "Error: File '$file' not found."
+fi
