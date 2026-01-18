8c00d08669eb776cc55766f6eecd12f88614d643
Vivesh <vivesht07@gmail.com>
2026-01-18 14:08:24 +0000

    task 25th completed

---
+++ Tasks/Basic-Level/25th.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE="server.log"
+
+if [ -f "$FILE" ]; then
+    echo "Enter the keyword you want to find:"
+    echo "For example "Error" "
+    read search_term
+
+    echo "--- Search Results ---"
+    grep -i "$search_term" "$FILE"
+else
+    echo "Error: $FILE not found."
+fi
