eed33c96ec396c4d54f556d4d9fc77ab1451d35f
vivs-ty <vivesht07@gmail.com>
2026-03-21 21:41:47 +0530

    task 8_9_10

--- Tasks/Basic-Level/8th.sh
+++ Tasks/Basic-Level/8th.sh
@@ -1,6 +1,12 @@
+# 8. List all files and directories in the current directory.
+
 #!/bin/bash
 
 echo -e "To list all non-hidden files and directories in the current directory, use the command:\nls\n--------------------"
+
 ls
+
 echo "--------------------"
+
+
 
--- Tasks/Basic-Level/9th.sh
+++ Tasks/Basic-Level/9th.sh
@@ -1,5 +1,9 @@
+# 9. List files with detailed information (permissions, owner, size).
+
 #!/bin/bash
 
 echo -e "To list all files with detailed information in the current directory, use the command:\nls -l\n--------------------"
+
 ls -l
+
 echo "--------------------"
