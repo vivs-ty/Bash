9229dff67d75965f1cc27b95e4264d9e2223a81c
vivs-ty <vivesht07@gmail.com>
2026-03-24 21:23:51 +0530

    Task 44 45 46 47

--- Tasks/Basic-Level/44th.sh
+++ Tasks/Basic-Level/44th.sh
@@ -1,3 +1,5 @@
+# 44. Create a `for` loop to iterate over a list of cloud providers: AWS, Azure, GCP.
+
 #!/bin/bash
 
 for provider in AWS Azure GCP; do
--- Tasks/Basic-Level/47th.sh
+++ Tasks/Basic-Level/47th.sh
@@ -1,3 +1,5 @@
+# 47. Create a `while` loop that reads lines from a file until the end is reached.
+
 #!/bin/bash
 
 while IFS= read -r line; do
