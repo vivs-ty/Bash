f40ca97d469686aab0c7a208587b3be5a1e0ffaf
vivs-ty <vivesht07@gmail.com>
2026-03-30 21:21:10 +0530

    updated 1 21 22 23 25 45

--- Tasks/Basic-Level/22nd.sh
+++ Tasks/Basic-Level/22nd.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 echo "Please enter the file name:"
-read file_name
+read -r file_name
 
 # 'head' shows the top of the file
 head -n 10 "$file_name"
--- Tasks/Basic-Level/23rd.sh
+++ Tasks/Basic-Level/23rd.sh
@@ -3,7 +3,7 @@
 #!/bin/bash
 
 echo "Please enter the file name:"
-read file_name
+read -r file_name
 
-# 'head' shows the top of the file
+# 'tail' shows the bottom of the file
 tail -n 5 "$file_name"
