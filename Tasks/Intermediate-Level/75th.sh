21fd87fd9637b65a907df43236291fbc5da26a6a
vivs-ty <vivesht07@gmail.com>
2026-04-13 21:50:30 +0530

    Task 75

---
+++ Tasks/Intermediate-Level/75th.sh
@@ -0,0 +1,8 @@
+# 75. Count the occurrences of each HTTP status code in an Nginx access log.
+
+#!/bin/bash
+
+echo "Counting the occurrences of each HTTP status code in the Nginx access log..."
+echo " use the command: awk '{print $9}' /var/log/nginx/access.log
+echo "Results:"
+awk '{print $9}' /var/log/nginx/access.log | sort | uniq -c | sort -nr
