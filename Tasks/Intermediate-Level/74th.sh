d16b32ba8863f380288addeb63511fed9cd9ad50
vivs-ty <vivesht07@gmail.com>
2026-04-13 21:53:48 +0530

    Task 75 74 73

--- Tasks/Intermediate-Level/74th.sh
+++ Tasks/Intermediate-Level/74th.sh
@@ -3,6 +3,6 @@
 #!/bin/bash
 
 echo "Extracting all unique IP addresses from the Apache access log..."
-echo " use the command: awk '{print $1}' /var/log/apache2/access.log
+echo " use the command: awk '{print $1}' /var/log/apache2/access.log "
 echo "Results:"
 awk '{print $1}' /var/log/apache2/access.log | sort | -u
