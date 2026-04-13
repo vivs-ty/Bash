592442e55b5f695a77c5af5bbb09fcebfab561dd
vivs-ty <vivesht07@gmail.com>
2026-04-13 21:53:28 +0530

    Task 75

---
+++ Tasks/Intermediate-Level/76th.sh
@@ -0,0 +1,8 @@
+# 76. Replace all occurrences of "localhost" with "127.0.0.1" in a configuration file.
+
+#!/bin/bash
+
+echo "Replacing all occurrences of 'localhost' with '127.0.0.1' in the configuration file..."
+echo " use the command: sed -i 's/localhost/127.0.0.1/g' /path/to/config.conf "
+echo "Results:"
+sed -i 's/localhost/127.0.0.1/g' /path/to/config.conf
