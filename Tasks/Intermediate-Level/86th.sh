10d419362962201e9c1d58a9fcf870ce39d009fe
vivs-ty <vivesht07@gmail.com>
2026-04-14 12:33:05 +0530

    Task 86

---
+++ Tasks/Intermediate-Level/86th.sh
@@ -0,0 +1,9 @@
+# 86. Extract the contents of a `tar.gz` archive.
+
+
+#!/bin/bash
+
+echo "Extracting the contents of the tar.gz archive..."
+echo " use the command: tar -xzf archive.tar.gz -C /path/to/destination "
+echo "Results:"
+tar -xzf archive.tar.gz -C /path/to/destination 
