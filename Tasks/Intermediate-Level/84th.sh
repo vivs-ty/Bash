216a153579fe08611e2e3b2a80526e084166bd88
vivs-ty <vivesht07@gmail.com>
2026-04-14 12:27:09 +0530

    Task 84

---
+++ Tasks/Intermediate-Level/84th.sh
@@ -0,0 +1,8 @@
+# 84. Apply a patch file to a configuration file.
+
+#!/bin/bash
+
+echo "Applying a patch file to the configuration file..."
+echo " use the command: patch /path/to/configuration.conf /path/to/patchfile.patch "
+echo "Results:"
+patch /path/to/configuration.conf /path/to/patchfile.patch 
