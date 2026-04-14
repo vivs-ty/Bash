fa9039ab260d635653898de56d08fab777afc7f4
vivs-ty <vivesht07@gmail.com>
2026-04-14 12:33:24 +0530

    Task 87

---
+++ Tasks/Intermediate-Level/87th.sh
@@ -0,0 +1,9 @@
+# 87. Compress a file using `gzip`.
+
+
+#!/bin/bash
+
+echo "Compressing the file using gzip..."
+echo " use the command: gzip /path/to/file.txt "
+echo "Results:"
+gzip /path/to/file.txt  
