5d70e952ac91506fdc704e3f8b72f568673cd4d8
vivs-ty <vivesht07@gmail.com>
2026-04-17 18:57:19 +0530

    Task 118

---
+++ Tasks/Intermediate-Level/118th.sh
@@ -0,0 +1,6 @@
+# 118. Create a script that reads environment variables for cloud credentials (e.g., `AWS_ACCESS_KEY_ID`).
+
+#!/bin/bash
+echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"
+echo "AWS Secret Access Key: $AWS_SECRET_ACCESS_KEY"
+echo "AWS Region: $AWS_REGION"
