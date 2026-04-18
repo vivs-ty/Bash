9e15163a8b11933452ca65772da4e0504fd90417
vivs-ty <vivesht07@gmail.com>
2026-04-18 18:54:20 +0530

    Task 121

---
+++ Tasks/Intermediate-Level/121st.sh
@@ -0,0 +1,9 @@
+# 121. Simulate listing "instances" (e.g., by listing files in a directory).
+
+
+#!/bin/bash
+
+echo "Simulating listing instances by showing files in the 'instances' directory:"
+echo "command used for listing files : ls -l instances/"
+echo "---------------------------------------------"
+ls -l instances/
