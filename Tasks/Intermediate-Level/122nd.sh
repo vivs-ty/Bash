09a3d53880bac1e26623a97a2daaa84f04969220
vivs-ty <vivesht07@gmail.com>
2026-04-18 18:56:13 +0530

    Task 122

---
+++ Tasks/Intermediate-Level/122nd.sh
@@ -0,0 +1,9 @@
+# 122. Simulate starting an "instance" (e.g., by creating an empty file).
+
+#!/bin/bash
+
+echo "Simulating starting an instance by creating an empty file named 'instance1' in the 'instances' directory:"
+echo "command used for creating file : touch instances/instance1"
+echo "---------------------------------------------"
+touch instances/instance1
+echo "Instance 'instance1' started (file created)."
