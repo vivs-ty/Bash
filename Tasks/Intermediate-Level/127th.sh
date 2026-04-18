544e71c9c2b62b8b3e83445d4cea4e5be60718ad
vivs-ty <vivesht07@gmail.com>
2026-04-18 19:00:57 +0530

    Task 127

---
+++ Tasks/Intermediate-Level/127th.sh
@@ -0,0 +1,10 @@
+# 127. Use `set -x` to debug a script by showing executed commands.
+
+#!/bin/bash
+
+echo "Using 'set -x' to enable debugging mode. This will show each command and its arguments as they are executed."
+echo "---------------------------------------------"
+set -x
+echo "This command will be shown in the debug output."
+ls non_existent_file.txt  # This command will fail, but the debug output will show it before it fails.
+echo "This command will still be executed even if the previous command fails, because we haven't set 'set -e'."
