144c0bcb6eb9c132582e1b7e881fb2a27cf3c441
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:55:11 +0530

    Task 147

--- Tasks/Intermediate-Level/147th.sh
+++ Tasks/Intermediate-Level/147th.sh
@@ -1,1 +1,12 @@
 # 147. Use Azure CLI to create and delete a virtual machine.
+
+#!/bin/bash
+VM_NAME="myVM"
+RESOURCE_GROUP="myResourceGroup"
+LOCATION="eastus"
+echo "Creating virtual machine $VM_NAME in resource group $RESOURCE_GROUP..."
+az vm create --resource-group $RESOURCE_GROUP --name $VM_NAME --image UbuntuLTS --location $LOCATION
+echo "Virtual machine $VM_NAME created successfully."
+echo "Deleting virtual machine $VM_NAME from resource group $RESOURCE_GROUP..."
+az vm delete --resource-group $RESOURCE_GROUP --name $VM_NAME --yes
+echo "Virtual machine $VM_NAME deleted successfully."
