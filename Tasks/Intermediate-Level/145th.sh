4342060fa366dcbdf3587a21e516b93d1d5172d2
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:53:20 +0530

    Task 145

--- Tasks/Intermediate-Level/145th.sh
+++ Tasks/Intermediate-Level/145th.sh
@@ -1,1 +1,5 @@
 # 145. Use AWS CLI to list all IAM users.
+
+#!/bin/bash
+echo "Listing all IAM users..."
+aws iam list-users --query 'Users[*].{UserName:UserName,UserId:UserId,CreateDate:CreateDate}' --output table
