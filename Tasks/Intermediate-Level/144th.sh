6b5d5ad237336f6cfb8bb0e73a0161d43da552e8
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:52:20 +0530

    Task 144

--- Tasks/Intermediate-Level/144th.sh
+++ Tasks/Intermediate-Level/144th.sh
@@ -1,1 +1,8 @@
 # 144. Use AWS CLI to upload a file to an S3 bucket.
+
+#!/bin/bash
+BUCKET_NAME="your-bucket-name"
+FILE_PATH="path/to/your/file.txt"
+echo "Uploading $FILE_PATH to S3 bucket: $BUCKET_NAME..."
+aws s3 cp $FILE_PATH s3://$BUCKET_NAME/
+echo "File uploaded successfully to s3://$BUCKET_NAME/$(basename $FILE_PATH)"
