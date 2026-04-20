24f7166505f582a87c8ff492a4d0c7f991a3c1e9
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:55:44 +0530

    Task 148

--- Tasks/Intermediate-Level/148th.sh
+++ Tasks/Intermediate-Level/148th.sh
@@ -1,1 +1,5 @@
 # 148. Use Azure CLI to list storage accounts.
+
+#!/bin/bash
+echo "Listing all storage accounts in the current Azure subscription..."
+az storage account list --query '[].{Name:name,ResourceGroup:resourceGroup,Location:location}' --output table
