059aa1a28ccf5ef087210454aaadb34d14fbabca
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:56:51 +0530

    Task 150

--- Tasks/Intermediate-Level/150th.sh
+++ Tasks/Intermediate-Level/150th.sh
@@ -1,1 +1,5 @@
 # 150. Use gcloud to list all compute instances in a project.
+
+#!/bin/bash
+echo "Listing all compute instances in the current Google Cloud project..."
+gcloud compute instances list --format="table(name,zone,machineType,status)"
