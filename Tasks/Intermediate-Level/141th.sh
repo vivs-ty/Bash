5a69acda7442a9a1b2d60c988fa5176cd1380b83
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:48:43 +0530

    Task 141

--- Tasks/Intermediate-Level/141th.sh
+++ Tasks/Intermediate-Level/141th.sh
@@ -1,1 +1,5 @@
 # 141. Use AWS CLI to list all EC2 instances in a specific region.
+
+#!/bin/bash
+echo "Listing all EC2 instances in the us-west-1 region..."
+aws ec2 describe-instances --region us-west-1 --query 'Reservations[*].Instances[*].{Instance:InstanceId,State:State.Name,Type:InstanceType}' --output table
