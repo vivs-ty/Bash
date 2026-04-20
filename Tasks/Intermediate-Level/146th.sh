7a2f54bfaab5f65e77fac5dc69539935cc0cb276
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:53:57 +0530

    Task 146

--- Tasks/Intermediate-Level/146th.sh
+++ Tasks/Intermediate-Level/146th.sh
@@ -1,1 +1,5 @@
 # 146. Use Azure CLI to list all resource groups.
+
+#!/bin/bash
+echo "Listing all resource groups in the current Azure subscription..."
+az group list --query '[].{Name:name,Location:location}' --output table
