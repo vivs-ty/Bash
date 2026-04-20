3f230cd12cffb4263de9dc44ae6189b5e1f3b057
vivs-ty <vivesht07@gmail.com>
2026-04-20 21:49:23 +0530

    Task 142

--- Tasks/Intermediate-Level/142nd.sh
+++ Tasks/Intermediate-Level/142nd.sh
@@ -1,1 +1,7 @@
 # 142. Use AWS CLI to start and stop an EC2 instance by its ID.
+
+#!/bin/bash
+echo "Starting EC2 instance with ID i-0123456789abcdef0..."
+aws ec2 start-instances --instance-ids i-0123456789abcdef0
+echo "Stopping EC2 instance with ID i-0123456789abcdef0..."
+aws ec2 stop-instances --instance-ids i-0123456789abcdef0
