ef43246762503930f5e0f45b3db283148b265770
vivs-ty <vivesht07@gmail.com>
2026-04-21 23:31:22 +0530

    Task 152

---
+++ Tasks/Intermediate-Level/152nd.sh
@@ -0,0 +1,6 @@
+# 152. Use gcloud to list all Cloud Storage buckets.
+
+#!/bin/bash
+
+echo "Listing all Cloud Storage buckets in the current Google Cloud project..."
+gcloud storage buckets list --format="table(name,location,storageClass)" 
