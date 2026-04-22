0120a7fa88000ff3085108c5d2e97b99e4aa0642
vivs-ty <vivesht07@gmail.com>
2026-04-22 17:54:48 +0530

    Task 161

---
+++ Tasks/Intermediate-Level/161st.sh
@@ -0,0 +1,7 @@
+# 161. Write a Bash script to initialize a Terraform project (`terraform init`).
+
+#! /bin/bash
+
+echo "Initializing Terraform project..."
+terraform init
+echo "Terraform project initialized."
