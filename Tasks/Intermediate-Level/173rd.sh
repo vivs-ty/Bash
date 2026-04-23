199663d9ad849cea0ff7bb662b3a613af10e0da9
vivs-ty <vivesht07@gmail.com>
2026-04-23 21:44:56 +0530

    Task 173

---
+++ Tasks/Intermediate-Level/173rd.sh
@@ -0,0 +1,8 @@
+# 173. Use `curl` to send a POST request to an API endpoint with JSON data.
+
+#!/bin/bash
+
+echo "Enter the API endpoint URL:"
+read url
+echo "Enter the JSON data to send (e.g., {\"key\":\"value\"}):"
+read json_data
