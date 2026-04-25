95886e84500359a7a76bc64a67c342f7435e5c51
vivs-ty <vivesht07@gmail.com>
2026-04-25 17:04:38 +0530

    Task 194

--- Tasks/Intermediate-Level/194th.sh
+++ Tasks/Intermediate-Level/194th.sh
@@ -1,1 +1,14 @@
 # 194. Use conditional debugging output based on a script argument or environment variable.
+
+#!/bin/bash
+
+
+echo "This script demonstrates conditional debugging output as follows:"
+if [[ "$1" == "-d" ]] || [[ "${DEBUG:-0}" == "1" ]]; then
+    echo "Debugging mode is ON. Detailed output will be shown."
+    # Add detailed debugging output here
+else
+    echo "Debugging mode is OFF. Running with normal output."
+    # Add normal output here
+fi
+echo "This is a normal message that will always be shown."
